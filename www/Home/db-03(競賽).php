341d007f42b84ad80760ecfb2f4088f0cf204a0c
public class <109534230@stu.ukn.edu.tw>
2024-11-18 09:20:46 +0800

    DELETE

--- www/Home/db-03(競賽).php
+++
@@ -1,9 +0,0 @@
-<?php
-/** 資料庫連線 */
-$link = mysqli_connect("127.0.0.1", "root", "", "testdb");
-if ($link) {
-    mysqli_query($link, 'SET NAMES UTF8');
-} else {
-    echo "資料庫連接失敗: " . mysqli_connect_error();
-}
-?>
--- www/Home/get_departments.php
+++
@@ -1,7 +0,0 @@
-<?php
-$school_id = $_GET['school_id'];
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=HCHJ', 'HCHJ', 'xx435kKHq');
-$statement = $pdo->prepare("SELECT department_id, department_name FROM Department WHERE school_id = ?");
-$statement->execute([$school_id]);
-echo json_encode($statement->fetchAll(PDO::FETCH_ASSOC));
-?>
