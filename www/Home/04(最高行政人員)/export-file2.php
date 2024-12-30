8796173554e72b7b66f4647d66ccd02c7914d83d
KUN <109534230@stu.ukn.edu.tw>
2024-12-30 08:59:58 +0800

    更新(匯出檔案(Word))

--- www/Home/04(最高行政人員)/export-file2.php
+++ www/Home/04(最高行政人員)/export-file2.php
@@ -1,45 +1,44 @@
 <?php
-// 手動加載 PHPWord 的必要文件
 require_once 'PHPWord-master/src/PhpWord/PhpWord.php';
 require_once 'PHPWord-master/src/PhpWord/IOFactory.php';
-
-
-// MySQL連線設定
+require_once 'PHPWord-master/src/PhpWord/Writer/Word2007.php';
 $host = '127.0.0.1';
 $dbname = 'HCHJ';
 $username = 'HCHJ';
 $password = 'xx435kKHq';
 
-// 建立MySQL連線
 $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// 查詢圖片數據
-$stmt = $pdo->query("SELECT image_name, image_data FROM photos");
-$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$options = isset($_POST['options']) ? $_POST['options'] : [];
+if (empty($options)) {
+    die("請選擇至少一個匯出選項！");
+}
 
 // 創建Word文件
 $phpWord = new \PhpOffice\PhpWord\PhpWord();
 $section = $phpWord->addSection();
 
-// 循環所有圖片並將其插入Word文檔
-foreach ($images as $image) {
-    $imageName = $image['image_name'];
-    $imageData = $image['image_data'];
-
-    // 將圖片保存為臨時文件
-    $tempImagePath = tempnam(sys_get_temp_dir(), 'image_');
-    file_put_contents($tempImagePath, $imageData);
-
-    // 將圖片插入Word
-    $section->addImage($tempImagePath, array('width' => 600, 'height' => 400));
-    $section->addText($imageName);
+foreach ($options as $option) {
+    if ($option == 'all' || $option == 'license') {
+        $stmt = $pdo->query("SELECT license_name, license_details FROM licenses");
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $section->addText("證照名稱: " . $row['license_name']);
+            $section->addText("詳細資料: " . $row['license_details']);
+        }
+    }
+    if ($option == 'all' || $option == 'competition') {
+        $stmt = $pdo->query("SELECT comp_name, comp_result FROM competitions");
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $section->addText("競賽名稱: " . $row['comp_name']);
+            $section->addText("成績: " . $row['comp_result']);
+        }
+    }
 }
 
-// 保存Word文檔
-$outputFile = 'exported_images.docx';
+$outputFile = 'exported_file.docx';
 $writer = new \PhpOffice\PhpWord\Writer\Word2007($phpWord);
 $writer->save($outputFile);
 
-echo "Word document created successfully: <a href='$outputFile'>$outputFile</a>";
+echo "Word 文件已成功生成: <a href='$outputFile'>$outputFile</a>";
 ?>
