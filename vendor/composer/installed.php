21b52e778b477404a75fdc7b85db24575e117fda
KUN <109534230@stu.ukn.edu.tw>
2025-01-08 09:31:52 +0800

    加入PHPWORD套件(感覺會成功！！)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '304478041992e4617f025e9994af3b4e3b5c04e7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '304478041992e4617f025e9994af3b4e3b5c04e7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpoffice/math' => array(
+            'pretty_version' => '0.2.0',
+            'version' => '0.2.0.0',
+            'reference' => 'fc2eb6d1a61b058d5dac77197059db30ee3c8329',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpoffice/math',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpoffice/phpword' => array(
+            'pretty_version' => '1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => '8392134ce4b5dba65130ba956231a1602b848b7f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpoffice/phpword',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
--- www/Home/04(最高行政人員)/export-file2.php
+++ www/Home/04(最高行政人員)/export-file2.php
@@ -1,34 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+require_once 'vendor/autoload.php';
 
-// 引入 PHPWord 的核心類和所有相關檔案
-require_once '/data/HCHJ/www/Home/lib/PhpWord/PhpWord.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Settings.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Style.php';
-
-// 引入集合類
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Collection/AbstractCollection.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Collection/Bookmarks.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Element/Cell.php';
-// 引入元素類
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Element/AbstractElement.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Element/Text.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Element/Table.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Element/Row.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Element/Cell.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Element/Image.php';
-
-// 引入寫入器相關檔案
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Writer/WriterInterface.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Writer/AbstractWriter.php';
-require_once '/data/HCHJ/www/Home/lib/PhpWord/Writer/Word2007.php';
-set_include_path(get_include_path() . PATH_SEPARATOR . '/data/HCHJ/www/Home/lib/PhpWord');
-
-require_once 'PhpWord.php';
-require_once 'Writer/WriterInterface.php';
-require_once 'Writer/AbstractWriter.php';
-require_once 'Writer/Word2007.php';
 use PhpOffice\PhpWord\PhpWord;
 
 $phpWord = new PhpWord();
