98b9a8526041059306461fc1864559ce18d0ab52
109534209 <109534209@stu.ukn.edu.tw>
2025-02-05 16:32:30 +0800

    Update index-01.php

--- www/Home/index-01.php
+++ www/Home/index-01.php
@@ -113,7 +113,9 @@ $result = mysqli_query($link, $query2);
                             <li class="nav-item">
                                 <a class="page-scroll" href="/~HCHJ/Home/Contestblog-01.php">比賽資訊</a>
                             </li>
-                            
+                            <li class="nav-item">
+                            <a href="/~HCHJ/Home/Secondtechnicalcampus00-1.php">管理員</a>
+                            </li>
                             <li class="nav-item">
                                 <a class="page-scroll" href="/~HCHJ/Home/Contest-history(學生).php">競賽紀錄</a>
                             </li>
