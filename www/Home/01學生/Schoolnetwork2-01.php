8fd06ce738fb335150e2de0aa049498f45735c72
109534209 <109534209@stu.ukn.edu.tw>
2025-02-24 14:50:04 +0800

    收尾

--- www/Home/01學生/Schoolnetwork2-01.php
+++ www/Home/01學生/Schoolnetwork2-01.php
@@ -17,7 +17,7 @@ $query = sprintf("SELECT * FROM user WHERE user = '%d'", mysqli_real_escape_stri
 $result = mysqli_query($link, $query);
 
 if (!isset($_SESSION['user'])) {
-    echo("<script>
+    echo ("<script>
                     alert('請先登入！！');
                     window.location.href = '/~HCHJ/index.html'; 
                   </script>");
@@ -27,46 +27,48 @@ if (!isset($_SESSION['user'])) {
 
 <!doctype html>
 <html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>二技校園網介紹科系</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="shortcut icon" type="image/x-icon" href="schoolimages/ukn.png">
-        <!-- Place favicon.ico in the root directory -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>二技科系</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- ========================= CSS here ========================= -->
-		<link rel="stylesheet" href="assets/css/bootstrap-5.0.0-alpha.min.css">
-        <link rel="stylesheet" href="assets/css/LineIcons.2.0.css">
-		<link rel="stylesheet" href="assets/css/animate.css">
-		<link rel="stylesheet" href="assets/css/tiny-slider.css">
-		<link rel="stylesheet" href="assets/css/glightbox.min.css">
-		<link rel="stylesheet" href="assets/css/main.css">
-    </head>
-    <body>
+    <link rel="shortcut icon" type="image/x-icon" href="schoolimages/ukn.png">
+    <!-- Place favicon.ico in the root directory -->
 
-        <!-- ========================= preloader start ========================= -->
-            <div class="preloader">
-                <div class="loader">
-                    <div class="ytp-spinner">
-                        <div class="ytp-spinner-container">
-                            <div class="ytp-spinner-rotator">
-                                <div class="ytp-spinner-left">
-                                    <div class="ytp-spinner-circle"></div>
-                                </div>
-                                <div class="ytp-spinner-right">
-                                    <div class="ytp-spinner-circle"></div>
-                                </div>
-                            </div>
+    <!-- ========================= CSS here ========================= -->
+    <link rel="stylesheet" href="assets/css/bootstrap-5.0.0-alpha.min.css">
+    <link rel="stylesheet" href="assets/css/LineIcons.2.0.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/tiny-slider.css">
+    <link rel="stylesheet" href="assets/css/glightbox.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+
+    <!-- ========================= preloader start ========================= -->
+    <div class="preloader">
+        <div class="loader">
+            <div class="ytp-spinner">
+                <div class="ytp-spinner-container">
+                    <div class="ytp-spinner-rotator">
+                        <div class="ytp-spinner-left">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                        <div class="ytp-spinner-right">
+                            <div class="ytp-spinner-circle"></div>
                         </div>
                     </div>
                 </div>
             </div>
-        <!-- preloader end -->
+        </div>
+    </div>
+    <!-- preloader end -->
 
-           <!-- ========================= header start ========================= -->
+     <!-- ========================= header start ========================= -->
      <header class="header navbar-area">
         <div class="container">
             <div class="row align-items-center">
@@ -142,198 +144,163 @@ if (!isset($_SESSION['user'])) {
     </header>
     <!-- ========================= header end ========================= -->
 
-
-        <!-- ========================= page-banner-section start ========================= -->
-        <section class="page-banner-section pt-75 pb-75 img-bg" style="background-image: url('assets/img/bg/common-bg.svg')">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="banner-content">
-                            <h2 class="text-white">二技科系</h2>
-                            <div class="page-breadcrumb">
-                                <nav aria-label="breadcrumb">
-                                    
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- ========================= page-banner-section end ========================= -->
-
-        <?php
-$servername = "127.0.0.1"; //伺服器ip或本地端localhost
-$username = "HCHJ"; //登入帳號
-$password = "xx435kKHq"; //密碼
-$dbname = "HCHJ"; //資料表名稱
-
-//建立連線
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-//確認連線成功或失敗
-if ($conn->connect_error) {
-    die("連線失敗" . $conn->connect_error);
-}
-//echo "連線成功";
-
-// 接收school_id參數
-$school_id = $_GET['school_id'];
-$department_id = $_GET['department_id'];
-$ID = isset($_POST["school_id"]) ? $_POST["school_id"] : NULL;
-
-// 抓取對應學校的科系
-$sql = "SELECT department_id ,department_name FROM Department WHERE school_id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $school_id);
-$stmt->execute();
-$result = $stmt->get_result();
-
-// 準備科系資料陣列
-$departments = array();
-
-if ($result && mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $departments[] = $row;
-    }
-    mysqli_free_result($result);
-}
-
-// 抓取學校名稱
-$sql_school = "SELECT school_name FROM School WHERE school_id = ?";
-$stmt_school = $conn->prepare($sql_school);
-$stmt_school->bind_param("i", $school_id);
-$stmt_school->execute();
-$result_school = $stmt_school->get_result();
-$school_name = $result_school->fetch_assoc()['school_name'];
-
-?>
-      <!-- ========================= service-section start ========================= -->
-      <body>
-      <section class="container mt-5 d-flex justify-content-center align-items-center flex-column">
-    <h2 class="text-center" style="font-size: 3em; line-height: 1.2;"><?= $school_name ?></h2><br>
-    <table class="table table-hover text-center" style="width: 50%; font-size: 1.4em; line-height: 1.5;">
-        <thead>
-            <tr>
-                <th>序號</th>
-                <th>科系名稱</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $index = 1; ?>
-            <?php foreach ($departments as $department) : ?>
-                <tr>
-                    <td><?= $index++ ?></td>
-                    <td><?= htmlspecialchars($department['department_name']) ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <a href="Schoolnetwork1-01.php" class="btn btn-secondary">返回上一頁</a>
-</section>
-</body>
-<!-- ========================= service-section end ========================= -->
-
-
-
-
-       <!-- ========================= client-logo-section start ========================= -->
-<section class="client-logo-section pt-100">
-            <div class="container">
-                <div class="client-logo-wrapper">
-                    <div class="client-logo-carousel d-flex align-items-center justify-content-between">
-                        <div class="client-logo">
-
-                        </div>
-                        <div class="client-logo">
-
-                        </div> 
-                        <div class="client-logo">
-
-                        </div>
-                        <div class="client-logo">
-
-                        </div>
-                        <div class="client-logo">
-
-                        </div>
-                        <div class="client-logo">
-
-                        </div>
-                        <div class="client-logo">
-
-                        </div>
-
+    <!-- ========================= page-banner-section start ========================= -->
+    <section class="page-banner-section pt-75 pb-75 img-bg"
+        style="background-image: url('assets/img/bg/common-bg.svg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="banner-content">
+                        <h2 class="text-white">二技科系</h2>
                         
                     </div>
                 </div>
             </div>
-        </section>
-        <!-- ========================= client-logo-section end ========================= -->
+        </div>
+    </section>
+    <!-- ========================= page-banner-section end ========================= -->
 
-        <!-- ========================= footer start ========================= -->
-        <footer class="footer pt-100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-3 col-lg-4 col-md-6">
-                        <div class="footer-widget mb-60 wow fadeInLeft" data-wow-delay=".2s">
-                            <a href="index-01.php" class="logo mb-30"><img src="schoolimages/uknlogo.png" alt="logo"></a>
-                            <p class="mb-30 footer-desc">©康寧大學資訊管理科五年孝班 洪羽白、陳子怡、黃瑋晴、簡琨諺 共同製作</p>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-6">
-                        <div class="footer-widget mb-1 wow fadeInLeft" data-wow-delay=".8s">
-                            
-                            <ul class="footer-contact"> 
-                                <h3>關於我們</h3>                                                        
-                                <p>(02)2632-1181/0986-212-566</p>                                
-                                    <p>台北校區：114 臺北市內湖區康寧路三段75巷137號</p>                             
-                            </ul>
-                            <style>
-                                .footer .row {
-                                display: flex;
-                                align-items: center; /* 垂直居中 */
-                                justify-content: space-between; /* 讓兩個區塊分居左右 */
-                                }
-                                .footer-widget {                                   
-                                text-align: right; /* 讓「關於學校」內容靠右對齊 */
-                                }
-                            </style>
-                        </div>
+    <?php
+    $servername = "127.0.0.1"; // 伺服器 IP 或 localhost
+    $username = "HCHJ"; // 資料庫帳號
+    $password = "xx435kKHq"; // 資料庫密碼
+    $dbname = "HCHJ"; // 資料庫名稱
+    
+
+    // 建立連線
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // 確認連線成功
+    if ($conn->connect_error) {
+        die("連線失敗: " . $conn->connect_error);
+    }
+
+    // 接收 school_id 參數
+    $school_id = isset($_GET['school_id']) ? intval($_GET['school_id']) : 0;
+
+    // 抓取對應學校的科系
+    $sql = "SELECT department_id,department FROM test WHERE school_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $school_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // 準備科系資料陣列
+    $departments = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $departments[] = $row;
+        }
+    }
+    $stmt->close();
+
+    // 抓取學校名稱 (改為從 Secondskill 表查詢)
+    $sql_school = "SELECT department FROM test WHERE department_id = ?";
+    $stmt_school = $conn->prepare($sql_school);
+    $stmt_school->bind_param("s", $school_id);
+    $stmt_school->execute();
+    $result_school = $stmt_school->get_result();
+    $school_name = ($result_school->num_rows > 0) ? $result_school->fetch_assoc()['name'] : "未知學校";
+    $stmt_school->close();
+    $conn->close();
+    ?>
+    <!-- ========================= service-section start ========================= -->
+
+    <body>
+        <section class="container mt-5 d-flex justify-content-center align-items-center flex-column">
+            <h2 class="text-center" style="font-size: 3em; line-height: 1.2;"><?= $school_name ?></h2><br>
+            <table class="table table-hover text-center" style="width: 50%; font-size: 1.4em; line-height: 1.5;">
+                <thead>
+                    <tr>
+                        <th>序號</th>
+                        <th>科系名稱</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $index = 1; ?>
+                    <?php foreach ($departments as $department): ?>
+                        <tr>
+                            <td><?= $index++ ?></td>
+                            <td><?= htmlspecialchars($department['department']) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <a href="Schoolnetwork1-01.php" class="btn btn-secondary">返回上一頁</a>
+        </section>
+    </body>
+    <!-- ========================= service-section end ========================= -->
+
+    <!-- ========================= footer start ========================= -->
+    <footer class="footer pt-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-4 col-md-6">
+                    <div class="footer-widget mb-60 wow fadeInLeft" data-wow-delay=".2s">
+                        <a href="index-01.php" class="logo mb-30"><img src="schoolimages/uknlogo.png" alt="logo"></a>
+                        <p class="mb-30 footer-desc">©康寧大學資訊管理科五年孝班 洪羽白、陳子怡、黃瑋晴、簡琨諺 共同製作</p>
                     </div>
                 </div>
+                <div class="col-xl-3 col-lg-4 col-md-6">
+                    <div class="footer-widget mb-1 wow fadeInLeft" data-wow-delay=".8s">
 
-                <div class="copyright-area">
-                    <div class="row align-items-center">
-                        <div class="col-md-6">
-                            <div class="footer-social-links">
-                                <ul class="d-flex">
-                                    <li><a href="https://www.facebook.com/UKNunversity"><i class="lni lni-facebook-filled"></i></a></li>
-                                    <li><a href="https://www.instagram.com/ukn_taipei/"><i class="lni lni-instagram-filled"></i></a></li>
-                                </ul>
-                            </div>
+                        <ul class="footer-contact">
+                            <h3>關於我們</h3>
+                            <p>(02)2632-1181/0986-212-566</p>
+                            <p>台北校區：114 臺北市內湖區康寧路三段75巷137號</p>
+                        </ul>
+                        <style>
+                            .footer .row {
+                                display: flex;
+                                align-items: center;
+                                /* 垂直居中 */
+                                justify-content: space-between;
+                                /* 讓兩個區塊分居左右 */
+                            }
+
+                            .footer-widget {
+                                text-align: right;
+                                /* 讓「關於學校」內容靠右對齊 */
+                            }
+                        </style>
+                    </div>
+                </div>
+            </div>
+
+            <div class="copyright-area">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <div class="footer-social-links">
+                            <ul class="d-flex">
+                                <li><a href="https://www.facebook.com/UKNunversity"><i
+                                            class="lni lni-facebook-filled"></i></a></li>
+                                <li><a href="https://www.instagram.com/ukn_taipei/"><i
+                                            class="lni lni-instagram-filled"></i></a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-        </footer>
-        <!-- ========================= footer end ========================= -->
+        </div>
+    </footer>
+    <!-- ========================= footer end ========================= -->
 
 
-        <!-- ========================= scroll-top ========================= -->
-        <a href="#" class="scroll-top">
-            <i class="lni lni-arrow-up"></i>
-        </a>
-        
-		<!-- ========================= JS here ========================= -->
-		<script src="assets/js/bootstrap.bundle-5.0.0.alpha-min.js"></script>
-		<script src="assets/js/contact-form.js"></script>
-        <script src="assets/js/count-up.min.js"></script>
-        <script src="assets/js/tiny-slider.js"></script>
-        <script src="assets/js/isotope.min.js"></script>
-        <script src="assets/js/glightbox.min.js"></script>
-        <script src="assets/js/wow.min.js"></script>
-		<script src="assets/js/imagesloaded.min.js"></script>
-		<script src="assets/js/main.js"></script>
-    </body>
+    <!-- ========================= scroll-top ========================= -->
+    <a href="#" class="scroll-top">
+        <i class="lni lni-arrow-up"></i>
+    </a>
+
+    <!-- ========================= JS here ========================= -->
+    <script src="assets/js/bootstrap.bundle-5.0.0.alpha-min.js"></script>
+    <script src="assets/js/contact-form.js"></script>
+    <script src="assets/js/count-up.min.js"></script>
+    <script src="assets/js/tiny-slider.js"></script>
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/glightbox.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/imagesloaded.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
 </html>
