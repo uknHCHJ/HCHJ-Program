47bc1440e2b83e7da396670bd86ff1ae953653f5
KUN <109534230@stu.ukn.edu.tw>
2025-02-27 16:10:19 +0800

    s

--- www/Home/00(管理員)/contact-00.php
+++ www/Home/00(管理員)/contact-00.php
@@ -414,7 +414,7 @@ $result = mysqli_query($link, $query);
         <div class="col-xl-3 col-lg-4 col-md-6">
           <div class="footer-widget mb-60 wow fadeInLeft" data-wow-delay=".2s">
             <a href="index-00.php" class="logo mb-30"><img src="schoolimages/uknlogo.png" alt="logo"></a>
-            <p class="mb-30 footer-desc">©康寧大學資訊管理科製作</p>
+            <p class="mb-30 footer-desc">©康寧大學資訊管理科五年孝班 洪羽白、陳子怡、黃瑋晴、簡琨諺 共同製作</p>
           </div>
         </div>
         <div class="col-xl-3 col-lg-4 col-md-6">
